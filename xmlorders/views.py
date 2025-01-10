9f1307af7abcc8bbf08f2dfa283528167336f99f
victoryus84 <victorcostiuc@gmail.com>
2025-01-10 16:15:46 +0200

    Add Django REST framework support with user and group serializers

--- xmlorders/views.py
+++ xmlorders/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import Group, User
 from .models import Order
 from .forms import OrderForm
 
-from rest_framework import viewsets
-from .serializers import OrderSerializer
+from rest_framework import permissions, viewsets
+from .serializers import *
 
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
 def create_order(request):
     if request.method == 'POST':
         form = OrderForm(request.POST)
