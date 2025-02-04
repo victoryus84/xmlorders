b46fe7d38344363576fb1a5688aeb42de7c9c681
victoryus84 <victorcostiuc@gmail.com>
2025-02-04 14:20:46 +0200

    Add token authentication and login endpoint; fix Dockerfile entrypoint path

--- backend/xmlorders/views.py
+++ backend/xmlorders/views.py
@@ -1,10 +1,25 @@
+from django.contrib.auth import authenticate
 from rest_framework import permissions, mixins
 from rest_framework.decorators import action
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND
 from .models import Order, Client, Product
 from .serializers import OrderSerializer, ClientSerializer, ProductSerializer
 
+class Login(APIView):
+
+    def post(self, request):
+        user = authenticate(username=request.data.get("username"), password=request.data.get("password"))
+        if not user:
+            return Response({'error': 'Credentials are incorrect or user does not exist'}, status=HTTP_404_NOT_FOUND)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=HTTP_200_OK)
+    
 class ClientViewSet(mixins.CreateModelMixin,
                    mixins.RetrieveModelMixin,
                    mixins.UpdateModelMixin,
