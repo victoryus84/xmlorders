5b56f7d83e9f561f1adbbc6927c59d7ba90e4ca1
victoryus84 <victorcostiuc@gmail.com>
2025-02-15 00:28:41 +0200

    Update API URL paths to version v1 for consistency and clarity

--- backend/xmlorders/urls.py
+++ backend/xmlorders/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 from .routers import router
 
 urlpatterns = [
-    path('api/', include(router.urls)),
-    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/v1/', include(router.urls)),
+    path('api/v1/auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
